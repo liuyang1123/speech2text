252ea774c156cfd4cf3dd519c7f49f638eac7014
nasrudin salim <nasdin94@hotmail.com>
2018-01-16 23:03:08 +0800

    fix bugs with environ_install

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # speech2text
 
 Please install tensorflow GPU version with the proper Cuda requirements after you're done running environ_install.sh via bash
+
+You'll have a new environment called deepspeech
--- environ_install.sh
+++ environ_install.sh
@@ -14,42 +14,31 @@ conda install -c anaconda gcc --yes
 conda install -c anaconda swig --yes
 conda install -c conda-forge importlib --yes
 conda install -c anaconda requests --yes
-yes | pip install deepspeech-gpu
+sudo apt-get install libcupti-dev --yes
 
 
-#install prerequisites for training
+
+#install prerequisites
 yes | pip install -r requirements.txt
 
 #So that you can train, specific version of tensorflow-gpu
+
 cd DeepSpeech
 python util/taskcluster.py --target /tmp --source tensorflow --arch gpu --artifact tensorflow_gpu_warpctc-1.4.0-cp27-cp27mu-linux_x86_64.whl
 yes | pip install /tmp/tensorflow_gpu_warpctc-1.4.0-cp27-cp27mu-linux_x86_64.whl
 
+#build native client with gpu binary
+python util/taskcluster.py --target . --arch gpu
 
-#build the native-client
-python util/taskcluster.py --target native-client/
 cd ..
-
-#checks for OS since some tards use Mac
-
-platform='unknown'
-unamestr=`uname`
-if [[ "$unamestr" == 'Linux' ]]; then
-   platform='linux'
-elif [[ "$unamestr" == 'Darwin' ]]; then
-   platform='mac'
-fi
-#then downloads some pre-built binaries
-if [[ $platform == 'linux' ]]; then
-   python DeepSpeech/util/taskcluster.py --target native-client/.
-elif [[ $platform == 'mac' ]]; then
-   python DeepSpeech/util/taskcluster.py --arch osx --target native-client/.
-fi
-
+#finally install the thing
+yes | pip install deepspeech-gpu
 
 #this one gets the pretrained models
 wget -O - https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/deepspeech-0.1.0-models.tar.gz | tar xvfz -
 
 
 #this one gets some training data
-python DeepSpeech/bin/import_cv.py data/common_voice/
+python DeepSpeech/bin/import_cv.py data/
+tar -xvzf data/cv_corpus_v1.tar.gz data/
+
