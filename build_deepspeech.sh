5a77e01ee3225acf9fa453bd34e64d2bc1cbcd78
nasrudin salim <nasdin94@hotmail.com>
2018-01-18 19:31:45 +0800

    remove DeepSpeech as a submodule

---
+++ build_deepspeech.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This one installs the new virtualenv environment
+echo "Building Deepspeech from source"
+echo "Getting the GPU version for python programming"
+sudo echo #So that it makes installation easier later
+
+
+
+
+
