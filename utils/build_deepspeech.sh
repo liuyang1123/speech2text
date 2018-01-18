37220996540e6c8b369db99903cf8c874f87d7bd
nasrudin salim <nasdin94@hotmail.com>
2018-01-18 19:38:51 +0800

    add deepspeech as package

--- utils/build_deepspeech.sh
+++ utils/build_deepspeech.sh
@@ -5,7 +5,16 @@ echo "Building Deepspeech from source"
 echo "Getting the GPU version for python programming"
 sudo echo #So that it makes installation easier later
 
+#remove the old one first
+rm -r DeepSpeech/
 
+#get latest Deepspeech
+git clone git@github.com:mozilla/DeepSpeech.git
+cd DeepSpeech
+## remove git stuff
+rm -f -r .git*
+
+exit
 
 
 
